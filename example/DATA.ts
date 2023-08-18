eb4477b3718e8e82ee18e27377f6b58d97f7a18a
softsuave-tech-matrix <techmatrix@softsuave.com>
2023-08-18 20:39:25 +0530

    feat: added an example

---
+++ example/DATA.ts
@@ -0,0 +1,94 @@
+import { type DynamicObject } from '../src/types';
+
+export const MOCK_DATA: DynamicObject[] = [
+  {
+    id: '1',
+    value: 'Sub Category 1',
+    category: 'Category 1',
+    date: 'Mar 12 2012 10:00:00 AM',
+  },
+  {
+    id: '2',
+    value: 'Sub Category 2',
+    category: 'Category 2',
+    date: 'Mar 12 2012 10:00:00 AM',
+  },
+  {
+    id: '3',
+    value: 'Sub Category 3',
+    category: 'Category 1',
+    date: 'Jan 12 2012 10:00:00 AM',
+  },
+  {
+    id: '4',
+    value: 'Sub Category 4',
+    category: 'Category 2',
+    date: 'Dec 12 2012 10:00:00 AM',
+  },
+  {
+    id: '5',
+    value: 'Sub Category 5',
+    category: 'Category 4',
+    date: 'Jun 12 2012 10:00:00 AM',
+  },
+  {
+    id: '6',
+    value: 'Sub Category 6',
+    category: 'Category 4',
+    date: 'May 12 2012 10:00:00 AM',
+  },
+  {
+    id: '7',
+    value: 'Sub Category 7',
+    category: 'Category 3',
+    date: 'Apr 12 2012 10:00:00 AM',
+  },
+  {
+    id: '8',
+    value: 'Sub Category 9',
+    category: 'Category 5',
+    date: 'Apr 12 2012 10:00:00 AM',
+  },
+  {
+    id: '10',
+    value: 'Sub Category 8',
+    category: 'Category 5',
+    date: 'Apr 12 2012 10:00:00 AM',
+  },
+  {
+    id: '11',
+    value: 'Sub Category 10',
+    category: 'Category 1',
+    date: 'Apr 12 2012 10:00:00 AM',
+  },
+  {
+    id: '12',
+    value: 'Sub Category 11',
+    category: 'Category 2',
+    date: 'Apr 12 2012 10:00:00 AM',
+  },
+  {
+    id: '13',
+    value: 'Sub Category 9',
+    category: 'Category 3',
+    date: 'Apr 12 2012 10:00:00 AM',
+  },
+  {
+    id: '14',
+    value: 'Sub Category 12',
+    category: 'Category 3',
+    date: 'Apr 12 2012 10:00:00 AM',
+  },
+  {
+    id: '15',
+    value: 'Sub Category 13',
+    category: 'Category 4',
+    date: 'Apr 12 2012 10:00:00 AM',
+  },
+  {
+    id: '16',
+    value: 'Sub Category 14',
+    category: 'Category 5',
+    date: 'Apr 12 2012 10:00:00 AM',
+  },
+];
